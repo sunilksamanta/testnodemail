15380ff664529bc856455ce65105737982f0c963
Sunil K Samanta <sunilthedj@gmail.com>
2018-03-29 11:33:25 +0530

    ttt

---
+++ server.js
@@ -0,0 +1,62 @@
+// Require express and create an instance of it
+var express = require('express');
+var app = express();
+const nodemailer = require('nodemailer');
+
+// on the request to root (localhost:3000/)
+app.get('/', function (req, res) {
+    res.send('<b>My</b> first express http server');
+});
+
+// On localhost:3000/welcome
+app.get('/testMail', function (req, res) {
+
+
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+    nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: account.user, // generated ethereal user
+                pass: account.pass // generated ethereal password
+            }
+        });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: 'sunilthedj@gmail.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    //console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+});
+});
+    //res.send('<b>Hello</b> welcome to my http server made with express');
+});
+
+// Change the 404 message modifing the middleware
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
+// start the server in the port 3000 !
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000.');
+});
